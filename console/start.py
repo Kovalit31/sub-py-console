a2730ebad97b2428495f9f8a8a9f8d2b74923a30
Kovalit31 <73341678+Kovalit31@users.noreply.github.com>
2022-03-29 12:25:44 +0300

    Non-working. I'm push for latest work

--- console/start.py
+++ console/start.py
@@ -8,17 +8,11 @@ def main():
             command_splitted = request_command.split(" ")
             continue_run = True
             try:
-                try:
-                    exec(f"{command_splitted[0]}.get_metadata()")
-                except:
-                    pass
-                exec(f"import {cfg.MODULE_BIN_PATH}{command_splitted[0]}")
+                exec(f'''import {cfg.MODULE_BIN_PATH}{command_splitted[0]}
+{command_splitted[0]}.main({command_splitted[1:]})
+''')
             except:
-                continue_run = False
                 print("No such command")
-            if continue_run:
-                if command_splitted[0] != "quit" and command_splitted[0] != "exit":
-                    exec(f"{command_splitted[0]}.main({command_splitted[1:]})")
         except KeyboardInterrupt:
             break
 
