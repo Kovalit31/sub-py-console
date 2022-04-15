781ae5fa516feecf9409afe30cb56326fb550831
Kovalit31 <nonecone20@gmail.com>
2022-04-15 14:42:37 +0000

    Downgrading v0.9.0-beta

--- console/bin/example.py
+++ console/bin/example.py
@@ -1,8 +1,5 @@
 def main(*args):
     print("Example\n")
 
-def get_metadata():
-    return {"version":"1.0.0", "description":"example","autority":"example Author"}
-
 if __name__ == '__main__':
     print("START THIS FILE IN CONSOLE!")
